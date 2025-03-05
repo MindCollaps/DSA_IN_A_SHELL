5782c091630d5b966bf2ab4f9eaf4b85636f0130
LilaLaunebaerli <lastmelodylp@gmail.com>
2025-03-05 15:38:40 +0100

    random room generator
    - i tried ;-;

--- game/Room.py
+++ game/Room.py
@@ -1,4 +1,6 @@
 from enum import Enum
+from npc import monster
+from item import Item
 
 
 class RoomType(Enum):
@@ -24,3 +26,27 @@ class Room:
 
     def generate_room(self):
         self.monsters.append("")
+    room_type_probabilities = {
+        RoomType.EMPTY = 0.4,
+        RoomType.ITEM = 0.2,
+        RoomType.BOSS = 0.1,
+        RoomType.MONSTER = 0.3
+    }
+
+self.Room_Type = random.choices(list(room_type_probabilities.keys()),
+            weights=list(room_type_probabilities.values()))[0]
+
+if self.Room_Type == RoomType.EMPTY:
+            self.description = "Ein leerer Raum. Hier gibt es nichts Besonderes."
+            self.monsters.append("")  # Leerer String für Räume ohne Monster
+
+elif self.Room_Type == RoomType.MONSTER:
+            monster = self.select_monster_by_rarity()
+            self.monsters.append(monster)
+            self.description = f"Ein düsterer Raum. {monster.name} lauert in der Ecke!"
+
+elif self.Room_Type == RoomType.ITEM:
+            item = getRandomItems(rarity: Rarity): Item[]
+            self.items.append(item)
+            self.description = f"Ein Raum mit einem Schatz! Du findest {item.name}."
+            self.monsters.append("")
